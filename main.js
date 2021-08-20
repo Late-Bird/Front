ce037ef271e6a0783bac54be2d11afda66af7d83
Late-Bird <87556325+Late-Bird@users.noreply.github.com>
2021-08-20 09:53:03 +0900

    JS : Arrow up

--- main.js
+++ main.js
@@ -42,7 +42,20 @@ document.addEventListener('scroll', () => {
     home.style.opacity =1 - window.scrollY / homeHeight;
 });
 
+// Show "Arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', ()=> {
+    if (window.scrollY > homeHeight /2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+})
 
+//Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () =>{
+    scrollIntoView('#home')
+});
 
 
 function scrollIntoView(selector) {
