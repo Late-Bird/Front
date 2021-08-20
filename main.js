fafb1ea0c4f106069a61f907df2ac5ec23922dfb
Late-Bird <87556325+Late-Bird@users.noreply.github.com>
2021-08-20 11:29:56 +0900

    JS : remove active

--- main.js
+++ main.js
@@ -67,6 +67,14 @@ workBtnContainer.addEventListener('click', (e) => {
     if(filter == null) {
         return;
     }
+
+    // Remobe selection from the previous item and select the new
+    const active = document.querySelector('.category__btn.selected')
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target :e.target.parentNode;
+    target.classList.add('selected');
+
+
     projectContainer.classList.add('anim-out');
     setTimeout(()=>{
         projects.forEach((project) => {
