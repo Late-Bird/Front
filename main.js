d227e31ae48a3405844d5e3ed5206b5ae167a801
Late-Bird <87556325+Late-Bird@users.noreply.github.com>
2021-08-20 09:39:28 +0900

    JS : Home opacity

--- main.js
+++ main.js
@@ -34,7 +34,20 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact')
 });
 
+
+// Make home slowly fade to transparent sd the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity =1 - window.scrollY / homeHeight;
+});
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior:'smooth'});
 };
+
+
