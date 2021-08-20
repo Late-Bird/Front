b16a2ebfba7d710128a840a2ff53723ab6c591d4
Late-Bird <87556325+Late-Bird@users.noreply.github.com>
2021-08-20 09:22:52 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ document.addEventListener('scroll', () => {
     }
     
 });
+
+
+// Handle scrolling when tappung on the navbar ment
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link==null) {
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+});
